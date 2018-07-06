555d9ac27066f56d2e3298469c869f40f634d992
Md Asif <asifmd786kgn@gmail.com>
2018-07-06 19:34:48 +0500

    init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path=require('path');
+const express=require('express');
+
+var publicPath=path.join(__dirname,'../public');
+var app=express();
+const port=process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+
+
+app.listen(port,()=>{
+  console.log('server is up on port 3000');
+});
