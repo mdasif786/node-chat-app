9ce2a0ed5b4b254ab5dfa60a9a0ea3deb05f96cf
Md Asif <asifmd786kgn@gmail.com>
2018-07-08 09:33:09 +0500

    add form for messages

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path=require('path');
 const http=require('http');
 const express=require('express');
 const socketIO=require('socket.io');
+const{generateMessage}=require('./utils/message');
 
 var publicPath=path.join(__dirname,'../public');
 var app=express();
@@ -12,23 +13,14 @@ var io=socketIO(server);
 
 io.on('connection',(socket)=>{
   console.log('new user connected');
-  socket.emit('newMessage',{
-    from: "Admin",
-    text: "welcome to the chat room"
-  });
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
-  socket.broadcast.emit("newMessage",{
-    from: "Admin",
-    text: "new user joined"
-  })
+  socket.broadcast.emit('newMessage',generateMessage('Admin','new user joined'));
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message,callback)=>{
     console.log('createMessage',message);
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('this is from the server');
   //socket.broadcast.emit()
   });
 
