c5d45e94221f509f8919ce00fe9b087fd6f2e688
Md Asif <asifmd786kgn@gmail.com>
2018-07-08 22:28:00 +0500

    add geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -18,11 +18,35 @@ socket.on('newMessage',function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocaionMessage',function(message){
+  var li=jQuery('<li></li>');
+  var a= jQuery('<a target="_blank">My Current Location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
   socket.emit('createMessage',{
     from: 'user',
     text: jQuery('[name=message]').val()
   },function(){
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation is not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+  socket.emit('createLocationMessage',{
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude
+  });
+  },function(){
+    alert('Unable to fetch location');
   });
 });
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path=require('path');
 const http=require('http');
 const express=require('express');
 const socketIO=require('socket.io');
-const{generateMessage}=require('./utils/message');
+const{generateMessage,generateLocationMessage}=require('./utils/message');
 
 var publicPath=path.join(__dirname,'../public');
 var app=express();
@@ -23,7 +23,10 @@ io.on('connection',(socket)=>{
     callback('this is from the server');
   //socket.broadcast.emit()
   });
+ socket.on('createLocationMessage',(coords)=>{
+   io.emit('newLocaionMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
 
+ });
   socket.on('disconnect',()=>{
     console.log('User was disconnected');
   });
